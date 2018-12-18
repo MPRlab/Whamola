873547db89eb71e6719182458c2f940af29d74bb
stoneil <seanogon@gmail.com>
2018-12-18 12:25:54 -0500

    updated .ini file to represent our board, added temporary script to copy firmware onto nucleo to get around bug, and added striker test code

--- src/strikers.cpp
+++ src/strikers.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "DualMC33926MotorShield.h"
+#include "QEI.h"
 
 // User button for shutoff
 DigitalIn stopButton(USER_BUTTON);
@@ -8,6 +9,7 @@ Serial pc(USBTX, USBRX);
 
 // Init striker motors
 DualMC33926MotorShield Dampener(D11, D15, A0, D3, D5);
+
 // DualMC33926MotorShield RightStriker(D8,D14);
 // DualMC33926MotorShield LeftStriker(D7,D13);
 
@@ -16,15 +18,18 @@ DualMC33926MotorShield Dampener(D11, D15, A0, D3, D5);
 //PwmOut * M1Pwm = new PwmOut(D15);
 
 
-int main(){
+int main(void){
 	pc.puts("Enabling\n");
 	Dampener.enable();
-	wait(2);
-	pc.puts("Sending a speed command\n");
-	Dampener.setM1Speed(0.5); // Set speed between -400.0 and 400.0
-	//M1Pwm->write(0.5f);
-	wait(2);
-	pc.puts("going into an infinite while loop\n");
-	while(1);
+	pc.puts("Sending a speed command, press user button to stop\n");
+	while(!stopButton){
+		Dampener.setM1Speed(0.9f); // Set speed between -1.0 to 1.0
+		pc.printf("current reading: %f Amps\tfault status: %d \n", Dampener.getM1CurrentAmps(), Dampener.hasFault());
+
+		//M1Pwm->write(0.5f);
+		wait(0.25);
+	}
+	pc.puts("stopping\n");
+	Dampener.setM1Speed(0.0f);
 	return 0;
 }
