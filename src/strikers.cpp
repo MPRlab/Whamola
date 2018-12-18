953a8e2e3f24cb50ccfb8528483c1e1c2e695ad7
stoneil <seanogon@gmail.com>
2018-12-18 16:35:52 -0500

    added function to calibrate dampener to have a zero position on the string

--- src/strikers.cpp
+++ src/strikers.cpp
@@ -1,8 +1,13 @@
 #include "mbed.h"
 #include "DualMC33926MotorShield.h"
-#include "QEI.h"
+#include <QEI.h>
+#include <PID.h>
 
 #define STRIKER_ENCODER_RESOLUTION 3200
+
+// Function prototypes
+void zeroDampenerOnString(void);
+void moveOneRotation(void);
 
 // User button for shutoff
 DigitalIn stopButton(USER_BUTTON);
@@ -16,28 +21,81 @@ QEI DampEnc(PD_1, PD_0, NC, STRIKER_ENCODER_RESOLUTION);
 // DualMC33926MotorShield LeftStriker(D7,D13);
 
 // Test
+Timer t;
 //PwmOut M1Pwm(D15);
 //PwmOut * M1Pwm = new PwmOut(D15);
 
 
 int main(void){
+	zeroDampenerOnString();
+	return 0;
+}
+
+void zeroDampenerOnString(){
+	float amps, revs;
+	float avgCurrent = 0.0f;
+	float currentSum = 0.0f;		
+	int ticks = 0;
+	int currentCount = 0;
+	pc.puts("Enabling\n");
+	Dampener.enable();
+	Dampener.setM1Speed(-0.1f); // Set speed between -1.0 to 1.0
+	t.start();
+
+	while(t.read() < 0.25){
+		amps = Dampener.getM1CurrentAmps();
+		currentSum += amps;
+		currentCount++;
+	}
+
+	avgCurrent = currentSum / currentCount;
+	float currentThreshold = avgCurrent + 0.05;
+	printf("current to detect zero point on the string: %f A\n", currentThreshold);
+	currentSum = 0.0;
+	currentCount = 0;
+
+	t.reset();
+	while(!stopButton && avgCurrent < currentThreshold){
+		amps = Dampener.getM1CurrentAmps();
+		currentSum += amps;
+		currentCount++;
+
+		if(t.read() > 0.25){
+			t.reset();
+			avgCurrent = currentSum / currentCount;
+			printf("avgCurrent: %f A\n", avgCurrent);
+			currentSum = 0;
+			currentCount = 0;
+		}
+
+	}
+	printf("avgCurrent: %f A\n", avgCurrent);
+
+	pc.puts("stopping\n");
+	Dampener.setM1Speed(0.0f);
+	DampEnc.reset();
+}
+
+void moveOneRotation(){
 	float amps, revs;		
 	int ticks;
 	pc.puts("Enabling\n");
 	Dampener.enable();
 	pc.puts("Sending a speed command, press user button to stop\n");
-	Dampener.setM1Speed(0.1); // Set speed between -1.0 to 1.0
+	Dampener.setM1Speed(0.7); // Set speed between -1.0 to 1.0
 
+	t.start();
 	while(!stopButton && abs(revs) < 1.0){
 		ticks = DampEnc.getPulses();
 		revs = (float) (ticks / STRIKER_ENCODER_RESOLUTION);
 		amps = Dampener.getM1CurrentAmps();
-		pc.printf("current reading: %f Amps\tfault status: %d \t Position: %d ticks\n", amps, Dampener.hasFault(), ticks);
 
+		if(t.read() > 0.4){
+			pc.printf("current reading: %f Amps\tfault status: %d \t Position: %d ticks\n", amps, Dampener.hasFault(), ticks);
+			t.reset();
+		}
 		//M1Pwm->write(0.5f);
-		wait(0.25);
 	}
 	pc.puts("stopping\n");
 	Dampener.setM1Speed(0.0f);
-	return 0;
 }
